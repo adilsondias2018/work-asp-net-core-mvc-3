e06d48c0a55aaa2c2a3fce6ebdd28e78d85922e9
adilsondias2018 <adilsondias@outlook.com>
2019-02-15 15:25:12 -0200

    Add project files.

---
+++ SalesWebMvc01/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SalesWebMvc01.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
